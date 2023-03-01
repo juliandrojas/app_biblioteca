4e47a07564ee76b19376a61fe87941439dedb5bc
Julián Rojas <juliandra140201@gmail.com>
2023-03-01 16:51:45 -0500

    getQueryData done

--- lib/src/ui/pages/home/pages/dbHelper/mongodb.dart
+++ lib/src/ui/pages/home/pages/dbHelper/mongodb.dart
@@ -1,9 +1,11 @@
 import 'dart:developer';
+import 'package:app_biblioteca/src/ui/pages/home/pages/controllers/search_book_controller.dart';
 import 'package:app_biblioteca/src/ui/pages/home/pages/dbHelper/constant.dart';
 import 'package:mongo_dart/mongo_dart.dart';
 
 class MongoDatabase {
   static var db, userCollection;
+  final SearchBookController controllerSearchBook = SearchBookController();
   //Create static connect function to connect with database
   static connect() async {
     db = await Db.create(MONGO_DB_URI);
@@ -13,8 +15,12 @@ class MongoDatabase {
   }
 
   //Create a function for displaying data
-  static Future<List<Map<String, dynamic>>> getQueryData() async {
-    final data = await userCollection.find().toList();
+  static Future<List<Map<String, dynamic>>> getQueryData(
+      SearchBookController controllerSearchBook) async {
+    //First field name (Column) second will be value.
+    final data = await userCollection
+        .find(where.eq('titulo', controllerSearchBook.myTextController.text))
+        .toList();
     return data;
   }
 }
