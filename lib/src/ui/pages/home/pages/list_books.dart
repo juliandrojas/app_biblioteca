64bbc6cf15357a14fd0faf120836a7ad793eec67
Julián Rojas <juliandra140201@gmail.com>
2023-03-01 16:58:21 -0500

    list_books initial commit

--- lib/src/ui/pages/home/pages/list_books.dart
+++ lib/src/ui/pages/home/pages/list_books.dart
@@ -15,32 +15,9 @@ class ListBooks extends StatelessWidget {
         title: const Text("Libros Disponibles"),
         backgroundColor: greenUTS,
       ),
-      body: SafeArea(
-        //Using FutureBuilder to fetching data
-        child: FutureBuilder(
-          future: MongoDatabase.getQueryData(),
-          builder: (context, AsyncSnapshot snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            } else {
-              if (snapshot.hasData) {
-                return const Center(
-                  child: Text("Data Found"),
-                );
-              } else {
-                return const Center(
-                  child: Text("Data not Found"),
-                );
-              }
-            }
-          },
-        ),
+      body: Center(
+        child: Text(controllerSearchBook.myTextController.text),
       ),
     );
-    /*Center(
-        child: Text(controllerSearchBook.myTextController.text),
-      ),*/
   }
 }
