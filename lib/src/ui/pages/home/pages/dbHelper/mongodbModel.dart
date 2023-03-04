36dc015a8de07ceda21f1bcc20611769d93711f5
Julián Rojas <juliandra140201@gmail.com>
2023-03-04 14:35:10 -0500

    Replace to object the camps in the json

--- lib/src/ui/pages/home/pages/dbHelper/mongodbModel.dart
+++ lib/src/ui/pages/home/pages/dbHelper/mongodbModel.dart
@@ -4,40 +4,42 @@
 
 import 'dart:convert';
 
+import 'package:mongo_dart/mongo_dart.dart';
+
 Welcome welcomeFromJson(String str) => Welcome.fromJson(json.decode(str));
 
 String welcomeToJson(Welcome data) => json.encode(data.toJson());
 
 class Welcome {
-    Welcome({
-        required this.id,
-        required this.titulo,
-        required this.autor,
-        required this.editorial,
-        required this.categoria,
-        required this.descripcion,
-        required this.prestadoA,
-    });
+  Welcome({
+    required this.id,
+    required this.titulo,
+    required this.autor,
+    required this.editorial,
+    required this.categoria,
+    required this.descripcion,
+    required this.prestadoA,
+  });
 
-    Id id;
-    String titulo;
-    String autor;
-    String editorial;
-    String categoria;
-    String descripcion;
-    String prestadoA;
+  ObjectId id;
+  Object titulo;
+  Object autor;
+  Object editorial;
+  Object categoria;
+  Object descripcion;
+  Object prestadoA;
 
-    factory Welcome.fromJson(Map<String, dynamic> json) => Welcome(
-        id: Id.fromJson(json["_id"]),
+  factory Welcome.fromJson(Map<String, dynamic> json) => Welcome(
+        id: json["_id"],
         titulo: json["titulo"],
         autor: json["autor"],
         editorial: json["editorial"],
         categoria: json["categoria"],
         descripcion: json["descripcion"],
         prestadoA: json["prestado_a"],
-    );
+      );
 
-    Map<String, dynamic> toJson() => {
+  Map<String, dynamic> toJson() => {
         "_id": id.toJson(),
         "titulo": titulo,
         "autor": autor,
@@ -45,21 +47,21 @@ class Welcome {
         "categoria": categoria,
         "descripcion": descripcion,
         "prestado_a": prestadoA,
-    };
+      };
 }
 
 class Id {
-    Id({
-        required this.oid,
-    });
+  Id({
+    required this.oid,
+  });
 
-    String oid;
+  String oid;
 
-    factory Id.fromJson(Map<String, dynamic> json) => Id(
+  factory Id.fromJson(Map<String, dynamic> json) => Id(
         oid: json["\u0024oid"],
-    );
+      );
 
-    Map<String, dynamic> toJson() => {
+  Map<String, dynamic> toJson() => {
         "\u0024oid": oid,
-    };
+      };
 }
