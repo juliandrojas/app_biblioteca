dc91c3d26f5bdeaa5a217e1ea615c552b4a8df38
Julián Rojas <juliandra140201@gmail.com>
2023-03-08 15:05:39 -0500

    Calling database connect when app start

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:app_biblioteca/src/app/app.dart';
 import 'package:app_biblioteca/src/app/firebase_options.dart';
+import 'package:app_biblioteca/src/ui/pages/home/pages/dbHelper/mongodb.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
@@ -7,5 +8,6 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   //Calling database connect when app start inside main()
+  await MongoDatabase.connect();
   runApp(const App());
 }
